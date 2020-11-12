73e265f91a7ad8f71cc983d147cd110a1ef24edc
David Dornyoh <daviddors12@gmail.com>
2020-11-12 18:29:25 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ Object.assign = require('object-assign')
 app.use(express.json());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
